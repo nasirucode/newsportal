48f2285e53fa1e1d677323625de6677d85ef766f
Akinkunmi Gbolahan Nasiru <65126751+Nasiru-code@users.noreply.github.com>
2020-06-05 15:07:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ include('includes/config.php');
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="akinkunmi nasiru">
 
     <title>News Portal | Home Page</title>
 
